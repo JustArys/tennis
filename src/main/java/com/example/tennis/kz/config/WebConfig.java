79d9b01e76aa4e284247905d373013e5a13f7e6c
JustArys <abiev.arystanbek@gmail.com>
2024-12-18 14:19:35 +0500

    Web Config

--- src/main/java/com/example/tennis/kz/config/WebConfig.java
+++ src/main/java/com/example/tennis/kz/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.tennis.kz.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9000")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedHeaders("*");
     }
 }
