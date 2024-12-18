3d26213778982bce75f45341cb65b1f0ac0a7f20
JustArys <abiev.arystanbek@gmail.com>
2024-12-18 14:21:38 +0500

    Web Config

--- src/main/java/com/example/tennis/kz/config/WebConfig.java
+++ src/main/java/com/example/tennis/kz/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.example.tennis.kz.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
